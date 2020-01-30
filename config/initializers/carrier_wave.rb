76d37f1051c0f0a1b9d99a569425103c3a81d056
Juakata <ha.uzquiano@hotmail.com>
2020-01-30 15:58:19 -0600

    Fix issue with href ids and protect secrets keys for google cloud

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/google'
     config.fog_credentials = {
         provider: 'Google',
-        google_storage_access_key_id: 'GOOG1EX7BOYWAPY5SDGIB3W73KX22N7FW47V4IZ5PLEWTGXSKJEG7ETMJOHHA',
-        google_storage_secret_access_key: 'kn6ECOmOvq6fwrdlTSMxZuQnJSY0pmB3RpeGYq1s'
+        google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
+        google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
     }
     config.fog_directory = 'portfolio-segmento-1'
 end
