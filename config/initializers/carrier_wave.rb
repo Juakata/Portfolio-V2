8414cfca50cc3bfbcf0ed1400b3ea4cebcfa65a9
Juakata <ha.uzquiano@hotmail.com>
2020-01-30 09:57:50 -0600

    add keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'Google',
-      google_storage_access_key_id: ENV['google_storage_access_key_id'],
-      google_storage_secret_access_key: ENV['google_storage_secret_access_key']
+      google_storage_access_key_id:  Rails.application.secrets.google_storage_access_key_id,
+      google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key
     }
     config.fog_directory = 'portfolio_segmento_1'
   end
