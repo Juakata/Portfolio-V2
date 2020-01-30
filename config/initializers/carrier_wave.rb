58beb8d83a5b13635ab741d3e98962e7bb3e5201
Juakata <ha.uzquiano@hotmail.com>
2020-01-30 12:11:50 -0600

    fix issues with google cloud services

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,9 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+    config.fog_provider = 'fog/google'
     config.fog_credentials = {
-      provider: 'Google',
-      google_storage_access_key_id:  Rails.application.secrets.google_storage_access_key_id,
-      google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key
+        provider: 'Google',
+        google_storage_access_key_id: Rails.application.secrets.private_key_id,
+        google_storage_secret_access_key: Rails.application.secrets.private_key
     }
-    config.fog_directory = 'portfolio_segmento_1'
-  end
+    config.fog_directory = Rails.application.secrets.google_cloud_storage_bucket_name
 end
