3f4e219cb69dfa42ed7ad48fa358b4fa8ebcc783
Juakata <ha.uzquiano@hotmail.com>
2020-02-04 16:24:55 -0600

    Add IntersectionObserver API

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,12 +4,4 @@ class Profile < ApplicationRecord
   mount_uploader :photo, PictureUploader
   belongs_to :user
   validate  :photo_size
-
-  private
-
-  def photo_size
-    if photo.size > 5.megabytes
-      errors.add(:photo, "should be less than 5MB")
-    end
-  end
 end
